f7f86f3c46e0846d5214c467ad93f677d94906a6
Sandino <sandinorodrigo@gmail.com>
2020-03-01 19:19:25 -0600

    Clase para conexión de base de datos

---
+++ ProyectoClinica/src/proyectoclinica/DB.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoclinica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class DB {
+   private static Connection cnx = null;
+   public static Connection conexion() throws SQLException, ClassNotFoundException {
+      if (cnx == null) {
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection("jdbc:mysql://localhost/clinica_unicah", "root", "");
+         } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+         } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+         }
+      }
+      return cnx;
+   }
+   public static void cerrar() throws SQLException {
+      if (cnx != null) {
+         cnx.close();
+      }
+   }
+}
