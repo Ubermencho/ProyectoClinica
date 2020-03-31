c3baf16bcba40a435ddf8c2b0647efd1a9d4066a
Oscar Flores <oflores482@gmail.com>
2020-03-31 16:43:59 -0600

    Ata ya funcion al 100%
    God pls smite me

--- ProyectoClinica/src/proyectoclinica/Medicamento.java
+++ ProyectoClinica/src/proyectoclinica/Medicamento.java
@@ -8,6 +8,8 @@ package proyectoclinica;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.swing.JOptionPane;
 
 /**
@@ -15,6 +17,31 @@ import javax.swing.JOptionPane;
  * @author oflor
  */
 public class Medicamento {
+    
+    private int cod_med, id_ata;
+    
+    public   Medicamento(){}
+    
+    public Medicamento(int med){
+        this.cod_med=med;
+    }
+
+    public int getCod_med() {
+        return cod_med;
+    }
+
+    public int getId_ata() {
+        return id_ata;
+    }
+
+    public void setCod_med(int cod_med) {
+        this.cod_med = cod_med;
+    }
+
+    public void setId_ata(int id_ata) {
+        this.id_ata = id_ata;
+    }
+    
     
     
     public int obtenercodMed(String Med) throws ClassNotFoundException{
@@ -24,6 +51,22 @@ public class Medicamento {
             ResultSet rs = combo.executeQuery("select * from medicamento where descripcion_medicamento= '"+Med+"'");
             rs.next();
             cod = rs.getInt("idmedicamento");
+        } catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return cod;
+    }
+    
+    public int obteneridATA(String doc, String nom, String ape, int edad, int car, String sin) throws ClassNotFoundException{
+        int cod=0;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime now = LocalDateTime.now();
+        try{            
+            Statement combo = DB.conexion().createStatement();
+            ResultSet rs = combo.executeQuery("select idata from ata where iddoctor='"+doc+"' and nombre_paciente_ata='"+nom+"' and apellido_paciente_ata='"+ape+"' and edad_paciente_ata= "+edad+" and idcarrera="+car+" and sintomas_paciente_ata='"+sin+"' and fecha_ata='"+dtf.format(now)+"';");
+            rs.next();
+            cod = rs.getInt("idata");
+            System.out.println(cod);
         } catch(SQLException e){
             JOptionPane.showMessageDialog(null, e);
         }
