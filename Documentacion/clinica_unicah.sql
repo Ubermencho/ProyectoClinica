8457e6df504d39a21c56ecf5782c8a9dde111c71
Oscar Flores <oflores482@gmail.com>
2020-04-01 14:22:59 -0600

    Script de la DB actualizado

--- Documentacion/clinica_unicah.sql
+++ Documentacion/clinica_unicah.sql
@@ -4,13 +4,23 @@ GRANT ALL ON clinica_unicah.* TO 'userClinica'@'localhost';
 
 USE `clinica_unicah`;
 
+CREATE TABLE `estado` (
+  `idestado` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion_estado` varchar(45) NOT NULL,
+  PRIMARY KEY (`idestado`),
+  UNIQUE KEY `idestado_UNIQUE` (`idestado`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `doctor` (
   `iddoctor` varchar(15) NOT NULL,
   `nombre_doctor` varchar(45) NOT NULL,
   `apellido_doctor` varchar(45) NOT NULL,
   `edad_doctor` varchar(45) NOT NULL,
+  `idestado` int(11) NOT NULL,
   PRIMARY KEY (`iddoctor`),
-  UNIQUE KEY `iddoctores_UNIQUE` (`iddoctor`)
+  UNIQUE KEY `iddoctores_UNIQUE` (`iddoctor`),
+  KEY `idestado_doctor_idx` (`idestado`),
+  CONSTRAINT `idestado_doctor` FOREIGN KEY (`idestado`) REFERENCES `estado` (`idestado`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `carrera` (
@@ -21,11 +31,14 @@ CREATE TABLE `carrera` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `medicamento` (
-  `idmedicamento` int(11) NOT NULL AUTO_INCREMENT,
+  `idmedicamento` varchar(45) NOT NULL,
   `descripcion_medicamento` varchar(100) NOT NULL,
   `tipo_medicamento` varchar(50) NOT NULL,
+  `idestado` int(11) NOT NULL,
   PRIMARY KEY (`idmedicamento`),
-  UNIQUE KEY `idmedicamento_UNIQUE` (`idmedicamento`)
+  UNIQUE KEY `idmedicamento_UNIQUE` (`idmedicamento`),
+  KEY `idestado_med_idx` (`idestado`),
+  CONSTRAINT `idestado_med` FOREIGN KEY (`idestado`) REFERENCES `estado` (`idestado`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `paciente` (
@@ -35,10 +48,14 @@ CREATE TABLE `paciente` (
   `apellido_paciente` varchar(30) NOT NULL,
   `edad_paciente` int(11) NOT NULL,
   `genero_paciente` varchar(10) NOT NULL,
+  `idestado` int(11) NOT NULL,
   PRIMARY KEY (`idpaciente`),
   UNIQUE KEY `idpaciente_UNIQUE` (`idpaciente`),
+  KEY `idcarrera_idx` (`idcarrera`),
   KEY `idcarrera_idcarrera` (`idcarrera`),
-  CONSTRAINT `idcarreraPaciente` FOREIGN KEY (`idcarrera`) REFERENCES `carrera` (`idcarrera`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `idestado_paciente_idx` (`idestado`),
+  CONSTRAINT `idcarreraPaciente` FOREIGN KEY (`idcarrera`) REFERENCES `carrera` (`idcarrera`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `idestado_paciente` FOREIGN KEY (`idestado`) REFERENCES `estado` (`idestado`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `ata` (
@@ -108,8 +125,12 @@ CREATE TABLE `usuario` (
   `fecha_creacion_usuario` datetime NOT NULL,
   `fecha_ultimo_ingreso_usuario` datetime DEFAULT NULL,
   `idtipo_usuario` int(11) NOT NULL,
+  `idestado` int(11) NOT NULL,
   PRIMARY KEY (`idusuario`),
   UNIQUE KEY `idusuario_UNIQUE` (`idusuario`),
   KEY `idtipo_usuario_idx` (`idtipo_usuario`),
+  KEY `idestado_usuario_idx` (`idestado`),
+  CONSTRAINT `idestado_usuario` FOREIGN KEY (`idestado`) REFERENCES `estado` (`idestado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `idtipo_usuario` FOREIGN KEY (`idtipo_usuario`) REFERENCES `tipo_usuario` (`idtipo_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
